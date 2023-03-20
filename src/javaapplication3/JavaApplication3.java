9d9e7234e9d45d0973fd5b5fe291666814821d30
20408 <20408@1k129s10.dune.edu>
2023-03-20 15:59:33 +0200

    Добавил Hello world

---
+++ src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author 20408
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Helo world!");
+        
+    }
+    
+}
