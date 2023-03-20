b8e03aa0e101f18de41595d1473d30e0f41bdf65
20408 <20408@1k129s10.dune.edu>
2023-03-20 16:00:01 +0200

    Добавил Hello

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -16,6 +16,7 @@ public class JavaApplication3 {
      */
     public static void main(String[] args) {
         System.out.println("Helo world!");
+        System.out.println("Helo!");
         
     }
     
